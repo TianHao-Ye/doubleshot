5e92d401d0b511772e34a8644e77a52e1898303b
TianHaoYe <yety@student.unimelb.edu.au>
2023-09-15 11:22:17 +1000

    wechat icon jump

--- script.js
+++ script.js
@@ -40,4 +40,22 @@ $(document).ready(function() {
   // Run the check on load and when the window is resized
   checkScreenWidth();
   $(window).resize(checkScreenWidth);
+
 });
+
+$(document).ready(function() {
+    // Attach the triggerJump function to the WeChat link click event
+    $("#wechat").click(function() {
+        // Find the element to animate (the one with the class 'jump-link')
+        const elementToAnimate = $(".jump-link");
+
+        // Add the 'jump' class to start the animation
+        elementToAnimate.addClass("jump");
+
+        // // After a delay, remove the 'jump' class to reset the animation
+        // setTimeout(function() {
+        //     elementToAnimate.removeClass("jump");
+        // }, 4000); // Adjust the delay as needed (1 second in this example)
+    });
+});
+
